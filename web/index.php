1ad81de53a6440e99e039af2ef5454077971646f
unknown <yova@student.undiksha.ac.id>
2024-07-02 22:05:22 +0800

    first commit

---
+++ web/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Docker Project</title>
+    <style>
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background: #333;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            color: #fff;
+        }
+
+        .header {
+            background-color: #444;
+            width: 100%;
+            padding: 20px 0;
+            text-align: center;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            position: fixed;
+            top: 0;
+            left: 0;
+        }
+
+        .header h1 {
+            margin: 0;
+            font-size: 2.5em;
+            font-weight: bold;
+            color: #fff;
+        }
+
+        .container {
+            background: rgba(255, 255, 255, 0.1);
+            padding: 30px;
+            border-radius: 15px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            text-align: center;
+            backdrop-filter: blur(10px);
+            margin-top: 100px;
+        }
+
+        .container p {
+            font-weight: bold;
+            font-size: 1.8em;
+            color: #fff;
+        }
+
+        .container p:first-of-type {
+            font-weight: bold;
+            font-size: 1.8em;
+            color: #fff;
+        }
+    </style>
+</head>
+<body>
+    <div class="header">
+        <h1>--PROJECT DOCKER--</h1>
+    </div>
+    <div class="container">
+        <p>PUTU YOVA PRADNYANA</p>
+        <p>2215101017</p>
+        <p>REG-A</p>
+    </div>
+</body>
+</html>
